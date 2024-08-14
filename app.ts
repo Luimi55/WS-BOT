9aed9c79b9103b981079bdb59ba0022d012d47a4
Luis Rosa <miguelrosa0105@gmail.com>
2024-08-14 18:41:37 -0400

    Feat: Migrate to typescript

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ const client = new Client({
     authStrategy: new LocalAuth()
 });
 
-var step = "1";
+let step : string = "1";
 
 client.on('ready', () => {
     console.log('Client is ready!');
@@ -24,11 +24,11 @@ client.on('message_create', message => {
             messageResponse = "Buenas tardes cliente que desea saber? \n 1. A que hora abren \n 2. Cuales son sus servicios"
             client.sendMessage(message.from, messageResponse);
             step = "2";
-        } else if(step == 2 && message.body == "!ping 1"){
+        } else if(step == "2" && message.body == "!ping 1"){
             messageResponse = "Abrimos a las 8:00 AM"
             client.sendMessage(message.from, messageResponse);
             step = "1";
-        } else if(step == 2 && message.body == "!ping 2"){
+        } else if(step == "2" && message.body == "!ping 2"){
             messageResponse = "Nuestros servicios son:\n 1. Manicure\n 2. Pedicure\n 3. Lavado de cabeza"
             client.sendMessage(message.from, messageResponse);
             step = "1";
