575ed51f70c47c4e2066b5bee999d3438c284e81
Luis Rosa <miguelrosa0105@gmail.com>
2024-08-16 20:52:30 -0400

    Feat: Develop logic to extract data from messages JSON and follow message flow

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+import type {Conversation} from './models/Conversation'
+import type {Message} from './models/Message'
+
 const qrcode = require('qrcode-terminal');
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
@@ -6,34 +9,98 @@ const client = new Client({
     authStrategy: new LocalAuth()
 });
 
-let step : string = "1";
+let messages: Message[] = [
+    {
+        id: "0",
+        text: "Hola elija una opcion:\nElejir 1\nElejir 2",
+        validValues: ["1", "2"]
+    },
+    {
+        id: "1",
+        text: "Opcion 1:\nElejir 1\nElejir 2",
+        validValues: ["1", "2"]
+    },
+    {
+        id: "2",
+        text: "Opcion 2\nElejir 1",
+        validValues: ["1"]
+    },
+    {
+        id: "1.1",
+        text: "Opcion 1.1:\nFin",
+        validValues: []
+    },
+    {
+        id: "1.2",
+        text: "Opcion 1.2:\nFin",
+        validValues: []
+    },
+    {
+        id: "2.1",
+        text: "Opcion 2.1:\nFin",
+        validValues: []
+    },
+
+]
+
+let conversations: Conversation[] = [];
+
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
-client.on('message_create', message => {
-    
-	if (message.body.includes('!ping')) {
-        let messageResponse = "";
-        if(step == "1"){
-            messageResponse = "Buenas tardes cliente que desea saber? \n 1. A que hora abren \n 2. Cuales son sus servicios"
-            client.sendMessage(message.from, messageResponse);
-            step = "2";
-        } else if(step == "2" && message.body == "!ping 1"){
-            messageResponse = "Abrimos a las 8:00 AM"
-            client.sendMessage(message.from, messageResponse);
-            step = "1";
-        } else if(step == "2" && message.body == "!ping 2"){
-            messageResponse = "Nuestros servicios son:\n 1. Manicure\n 2. Pedicure\n 3. Lavado de cabeza"
-            client.sendMessage(message.from, messageResponse);
-            step = "1";
+client.on('message_create', msg => {
+    try {
+        const conversation: Conversation | undefined = conversations.find(conv => conv.userId == msg.from);
+        if (msg.body.includes('!nexo')) {
+
+            if (conversation === undefined) {
+                conversations.push({
+                    userId: msg.from,
+                    lastMessageId: "0"
+                })
+                client.sendMessage(msg.from, messages[0].text)
+            }
+
+        } else {
+            if (conversation != undefined) {
+                let lastSentMessage: Message | undefined = messages.find(messsage => messsage.id == conversation.lastMessageId);
+                let isValidResponse: boolean | undefined = lastSentMessage?.validValues.some(val => val == msg.body);
+                if (isValidResponse) {
+                    let nextMessage: Message | undefined;
+                    let nextMessageId: string;
+                    if (lastSentMessage?.id == "0") {
+                        nextMessageId = msg.body;
+                        nextMessage = messages.find(messsage => messsage.id == msg.body);
+                    } else {
+                        nextMessageId = `${conversation.lastMessageId}.${msg.body}`;
+                        nextMessage = messages.find(messsage => messsage.id == nextMessageId);
+                    }
+                    if(nextMessage != undefined){
+                        client.sendMessage(conversation.userId, nextMessage.text);
+                        conversations.map(newConv => {
+                                if (newConv.userId = conversation.userId) {
+                                    newConv.lastMessageId = nextMessage.id
+                                }
+                            }
+                        )
+                    } else {
+                        // Log: Error tecnico: El id nextMessageId no esta configurado en el JSON de mensajes
+                        //client.sendMessage(conversation.userId, 'Ha ocurrido un error. Favor intentar en unos segundos.');
+                    }
+                } else {
+                    //client.sendMessage(conversation.userId, 'Esa opción no existe');
+                }
+            }
         }
-	}
+    } catch (ex) {
+        console.log(ex)
+    }
 });
 
 client.initialize();
---
+++ models/Conversation.ts
@@ -0,0 +1,4 @@
+export interface Conversation {
+    userId : string,
+    lastMessageId: string
+}
